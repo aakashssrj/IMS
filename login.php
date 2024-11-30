69ff50a0c1fa7f91104cbfcc49d071f684b358ec
Aakash Sah <aakashsah1436@gmail.com>
2024-11-30 23:47:25 +0530

    first_change

--- login.php
+++ login.php
@@ -1,107 +1,36 @@
-<?php
-
-$success = 0;
-$user = 0;
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  include 'connect.php';
-
-
-
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-
-  // $sql = "insert into registration(username,password) values('$username','$password')";
-
-  // $result = $conn->query($sql);
-
-  // if (!$result) {
-  //   die("connection Failed" . $conn->error);
-  // } else {
-  //   echo "Sign Up Successfully";
-  // }
-
-
-  $sql = "Select * from registration where username = '$username' and password = '$password'";
-  $result = $conn->query($sql);
-
-  if($result){
-    $num = mysqli_num_rows($result);
-    if($num >  0){
-      $user = 1;
-            session_start();
-            $_SESSION['username'] = $username;
-            header('Location : home.php');
-    }
-    else{
-            $user = 0;
-    }
-  }
-}
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-  <title>LogIn</title>
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>IMS Login Inventory Management System</title>
+  <link rel="stylesheet" href="css/login.css">
 </head>
 
 <body>
+  <div class="container">
+    <div class="loginHeader">
+      <h1>IMS</h1>
+      <p>Inventory Managment System</p>
+    </div>
 
-<?php
-if(!$user){
-  echo'<div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Sorry</strong> Wrong Inputed Data
-  <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"><span aria-hidden = "true">&times;</span></button>
-</div>';
- }
-?>
-
-
-<?php
-if($user){
-  echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong>Hurray !!</strong> Successfully Loged In
-  <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"><span aria-hidden = "true">&times;</span></button>
-</div>';
-}
-?>
-
-
-
-
-<div class="container mt-5">
-    <h1 class="text-center m-5">LogIn Page</h1>
-    <form action="login.php" method="POST">
-      <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Username</label>
-        <input type="text" class="form-control " name="username">
-      </div>
-      <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control " name="password">
-      </div>
-
-      <button type="submit" class="btn btn-primary w-100">Login</button>
-    </form>
-
-</div>
-
-
-
-
-
+    <div class="loginBody">
+      <form action="login.php" method="POST">
+        <div class="loginInputContainer">
+          <label for="">Username</label>
+          <input type="text" placeholder="Username" />
+        </div>
+        <div class="loginInputContainer">
+          <label for="">Password</label>
+          <input type="password" placeholder="password"/>
+        </div>
+        <div class="loginButton">
+          <button>Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </body>
 
 </html>
